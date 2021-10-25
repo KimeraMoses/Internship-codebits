e07d9a55c7bc3645f6437b72a155b3f4e2b084c5
Moxhus <kimeramoses001@gmail.com>
2021-10-25 23:30:41 +0300

    Python tasks

--- Python/app.py
+++ Python/app.py
@@ -80,3 +80,18 @@ def count_substring(string, sub_string):
         if (string[i:i+len(sub_string)] == sub_string):
             count = count + 1
     return count
+
+#*****Percentage
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    marks=0
+    for i in student_marks[query_name]:
+        marks=marks+i
+    avg=marks/3
+    print("%.2f"%avg)
