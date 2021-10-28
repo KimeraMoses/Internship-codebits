a90f35cedf299c564c28a7001653c360b55c29b4
Moxhus <kimeramoses001@gmail.com>
2021-10-28 10:44:30 +0300

    Tuples in python completed

--- Python/app.py
+++ Python/app.py
@@ -116,3 +116,10 @@ if __name__ == '__main__':
             MyList.sort();
         else:
             MyList.reverse();
+#****TUPLES
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    res = tuple(integer_list)
+    result = hash(res)
+    print(result)
